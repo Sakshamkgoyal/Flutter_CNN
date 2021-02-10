7ae82b5f3062895c4d34ee65416eeb02184f0508
Skkarrty <50166955+Sakshamkgoyal@users.noreply.github.com>
2021-02-10 15:27:32 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/catsdog/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.catsdog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/splashscreen.dart
@@ -0,0 +1,23 @@
+import 'package:catsdog/home.dart';
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class MySplash extends StatefulWidget {
+  @override
+  _MySplashState createState() => _MySplashState();
+}
+
+class _MySplashState extends State<MySplash> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 2,
+      navigateAfterSeconds: Home(),
+      title: Text('Dog and Cat', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30, color: Color(0xffe99600)),),
+      image: Image.asset('assets/1.png'),
+      backgroundColor: Colors.black,
+      photoSize: 50,
+      loaderColor: Color(0xffEEDA28),
+    );
+  }
+}
